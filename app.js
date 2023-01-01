ef32644bfc10d421cbc3de996e140f37080173f7
hyeong08 <gudtjq9735@gmail.com>
2023-01-01 16:11:54 +0900

    공부 내용 추가

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ const app = express()
 const port = 3000
 
 app.use(express.json())
-app.use(express.urlencoded({extended: false}))
 // extended: false로 옵션을 주면 NodeJs에 기본으로 내장된 querystring모듈을 사용
+app.use(express.urlencoded({extended: false}))
 
+// 쿼리스트링으로 데이터 주고 받는다
 app.get('/querystring', (req,res) => {
     // const page = req.query.page || 1
     //  || 뜻 or (해석하면 pageㅇㅇ 없으면 1로 가라)
@@ -28,11 +29,15 @@ app.post('/body-multipart', (res,req) => {
     res.send('multipart')
 })
 
-app.post('/body-json', (req,res) => {
-    // console.log(req.body)
-    res.send('json')
-})
 
+// 데이터를 보낼때 body에 json형식으로 넣고 보냄
+app.post('/body-json', (req,res) => {
+    const {id, password} = req.body // 객체구조분해할당 적용해봄.
+    console.log({id, password})
+    // console.log(req.body)
+    // res.send({id, password})
+    res.send(req.body)
+})
 
 
 app.listen(port, () => {
